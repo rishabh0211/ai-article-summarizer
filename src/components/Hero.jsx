6eed3151b83b08d695adfac2164b6ae9e58ed544
Rishabh Rastogi <rishabh0211@gmail.com>
2023-05-02 03:30:18 +0530

    hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex items-center justify-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="Logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          className="black_btn"
+          onClick={() => window.open("https://github.com/rishabh0211")}
+        >
+          Github
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
